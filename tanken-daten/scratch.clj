8ac4c1d3ebd1d0cf239d009515a6d6b5afe1be40
Michael Frericks <Michael-Frericks@web.de>
2013-10-09 23:41:49 +0200

    first xy-plot

--- tanken-daten/scratch.clj
+++ tanken-daten/scratch.clj
@@ -1,4 +1,5 @@
 (use 'tanken-daten.collect  :reload-all)
+(use '(incanter core stats charts))
 
 (def db-file "/home/michael/Dokumente/Tankstellenpreise/preise.db")
 
@@ -11,7 +12,22 @@
 (def s "Super")
 (def s10 "Super E10")
 
-(->> db-file
-     (select shell s)
-     ;(partition 2 1)
-     )
+(def p (->> db-file
+       (select shell s)
+       ((fn [l] (concat l (list [(System/currentTimeMillis) (second (last l)) ]))))
+       (partition 2 1)
+       (map (fn [[[t1 v1] [t2 v2]]] (vector [t1 v1] [(- t2 1) v2])))
+       (apply concat)
+       ))
+
+p
+
+(defn x [p] (map first p))
+(defn y [p] (map second p))
+
+(def v (xy-plot (x p) (y p)))
+
+(view v)
+
+
+
