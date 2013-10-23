4e8413df869039161a236603a5f31d4c52078608
Michael Frericks <Michael-Frericks@web.de>
2013-10-23 23:16:16 +0200

    Label at time-series-plot

--- tanken-daten/scratch.clj
+++ tanken-daten/scratch.clj
@@ -1,33 +1,58 @@
-(use 'tanken-daten.collect  :reload-all)
-(use '(incanter core stats charts))
+(ns scratch
+(:use (incanter core stats charts))
+(:require [clojure.pprint]
+          [tanken-daten.collect :as tdc] :reload-all))
+
 
 (def db-file "/home/michael/Dokumente/Tankstellenpreise/preise.db")
 
-(collect-data db-file)
+;;(def db-file "U:/Userdaten/Ablage/Tankstellenpreise/preise.db")
 
-(def shell "Shell, ALBERSLOHER WEG 415")
-(def ratio "Shell, ALBERSLOHER WEG 415")
-(def westfalen "Westfalen, Albersloher Weg 580")
+(tdc/collect-data db-file)
 
-(def s "Super")
-(def s10 "Super E10")
+(defn preise [db-file tankstelle sorte]
+    (->>   db-file
+           (clojure.java.io/reader)
+           java.io.PushbackReader.
+           (clojure.edn/read )
+           (filter #(= sorte (:treibstoff %)))
+           (sort-by (juxt :datum-zeit :tankstelle ))
+           (map #(update-in % [:tankstelle] (comp str first)))
+           (filter #(= tankstelle (:tankstelle %)))
+           ;;(map #(update-in % [:datum-zeit] (fn [d] (format "%1$Td %1$TH:%1$TM" d))))
+           (map #(update-in % [:datum-zeit] (fn [d] (.getTime d))))
+           ;;(map #(select-keys % [:tankstelle :preis :datum-zeit]))
+           (map (juxt :datum-zeit :preis) )
+           (partition 2 1)
+           (map (fn [[[t1 v1][t2 v2]]] [[t1 v1] [(- t2 1) v1]]))
+           (apply concat)
+           ))
 
-(def p (->> db-file
-       (select shell s)
-       ((fn [l] (concat l (list [(System/currentTimeMillis) (second (last l)) ]))))
-       (partition 2 1)
-       (map (fn [[[t1 v1] [t2 v2]]] (vector [t1 v1] [(- t2 1) v2])))
-       (apply concat)
-       ))
+(defn x-vals [list-of-x-y-pairs]
+  (->> list-of-x-y-pairs (map first)) )
 
-p
+(defn y-vals [list-of-x-y-pairs]
+  (->> list-of-x-y-pairs (map second)) )
 
-(defn x [p] (map first p))
-(defn y [p] (map second p))
+(def treibstoff "Super")
 
-(def v (xy-plot (x p) (y p)))
+(def w-super (preise db-file "W" treibstoff))
+(def r-super (preise db-file "R" treibstoff))
+(def s-super (preise db-file "S" treibstoff))
+
+(def v (time-series-plot (x-vals r-super) (y-vals r-super)  :title (str "Preis je Liter " treibstoff) :x-label "Zeit" :y-label "EUR"))
+;(def v (time-series-plot (x-vals w-super) (y-vals w-super)))
+;(def v (time-series-plot (x-vals s-super) (y-vals s-super)))
+
+:;r-super
+;(add-lines v (x-vals r-super) (y-vals r-super))
+
+(add-lines v (x-vals w-super) (y-vals w-super))
+(add-lines v (x-vals s-super) (y-vals s-super))
+
 
 (view v)
+
 
 
 
