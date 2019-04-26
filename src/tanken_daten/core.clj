edc890785ab491a699be8ded45c2ca0ce2a63433
Michael Frericks <Michael-Frericks@web.de>
2019-04-26 18:17:55 +0200

    Startet system

--- src/tanken_daten/core.clj
+++ src/tanken_daten/core.clj
@@ -1,16 +1,10 @@
 (ns tanken-daten.core
-  (:require [tanken-daten.collect :as c]
-[tanken-daten.adac-reader :as adac-reader ]))
+  (:require [tanken-daten.system  :as system ]))
 
 
 (defn -main
-  "Loops over collect-data. Waits 17 min between the calls of collect-data.
-  args[0] is the path to the database file"
-  [& args]
-  (println args)
-  (let [db-file (first args)]
-    (while true
-       (println "Collect at: " (java.util.Date.))
-       (prn (adac-reader/collect-data adac-reader/adac-ids))
-       (Thread/sleep (* 5000  #_60 #_17)))))
+  "Start the system"
+  []
+  (let [s (system/system)]
+  (system/start s )))
 
